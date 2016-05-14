1c95f390d51d1ee71ae009461b5ecf63680a344a
Yoshiyasu Kitahara <yoshi.kitahara.niapat@gmail.com>
2016-05-15 08:10:48 +0900

    Initial commit

---
+++ main.py
@@ -0,0 +1,86 @@
+#!/usr/bin/python
+#
+# Copyright 2007 Google Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+import webapp2
+
+form = """
+<form method="post">
+	What is your birthday?
+	<br>
+
+	<label>Month
+		<input type="text" name="month">
+	</label>
+	
+	<label>Day
+		<input type="text" name="day">
+	</label>
+	
+	<label>Year
+		<input type="text" name="year">
+	</label>
+
+	<br>
+	<br>
+	<input type="submit">
+</form>
+"""
+
+class MainPage(webapp2.RequestHandler):
+	def get(self):
+		self.response.out.write(form)
+
+      
+	def valid_month(month):
+		months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+		abbvs_month = dict((m[:3].lower(), m) for m in months)
+		if month:
+			short_month = month[:3].lower()
+			return abbvs_month.get(short_month)
+
+	def valid_day(day):
+		if day and day.isdigit():
+			int_day = int(day)
+			if 0 < int_day and int_day < 31:
+				return int_day
+			return None
+
+	def valid_year(year):
+		if year and year.isdigit():
+			int_year = int(year)
+			if 1900 <= int_year and int_year <= 2020:
+				return int_year
+			return None
+
+
+	def post(self):
+		user_month = valid_month(self.request.get("month"))
+		user_day = valid_day(self.request.get("day"))
+		user_year = valid_year(self.request.get("year"))    	
+
+		if not (user_month and user_day and user_year):
+			self.response.out.write(form)
+		else:
+			self.response.out.write("Thanks!")
+
+
+app = webapp2.WSGIApplication([('/', MainHandler)], debug=True)
+
+
+
+
+
+
